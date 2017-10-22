72fbff1ce63db423bc5bd8325cc07676b26a6f73
hanbin <han_bin@foxmail.com>
2017-10-23 00:25:39 +0800

    项目创建

---
+++ pre-server/src/main/java/cn/com/hanbinit/PreServerApplication.java
@@ -0,0 +1,12 @@
+package cn.com.hanbinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreServerApplication.class, args);
+	}
+}
